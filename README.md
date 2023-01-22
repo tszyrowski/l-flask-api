3e85c761593f075ed4f7d0251c92c46b341c01ec
tomasz szyrowski <tszyrowski@gmail.com>
2023-01-22 16:35:07 +0000

    main: Generate script for flsk-app with single db
    What:
    - The scripts provides requests routes for users
    - It creates sqlitedb on the first request
    - It provides route for createing the user
    Why:
    - To create app back-bone
    Where:
    https://www.youtube.com/watch?v=N0TvrhS9hc8&list=PL7g1jYj15RUP_Mri9ym6BdUais6_jvhrS&index=3

--- README.md
+++ README.md
@@ -3,3 +3,15 @@ Data Structure with Flask API
 
 Based on:
 https://www.youtube.com/watch?v=lOBu2zwxAm8&list=PL7g1jYj15RUP_Mri9ym6BdUais6_jvhrS
+
+# Create user:
+
+```
+curl --location --request POST 'http://127.0.0.1:5000/user' --header 'Content-Type: application/json' --data-raw '{"name": "Bill", "email": "bil@example.com", "address": "tomss address", "phone": "+ 4412345667"}'  
+```
+output:
+```
+{
+  "message": "User created"
+}
+```
--- server.py
+++ server.py
@@ -1,2 +1,110 @@
-from flask import Flask, request, jsonify
+from datetime import datetime
+from sqlite3 import Connection as SQLite3Connection
 
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import event
+from sqlalchemy.engine import Engine
+
+# app
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+# app.config["SQL_TRACK_MODIFICATIONS"] = 0
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = 0
+
+
+# configure sqlite3 to enforce foreign key constrains
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
+
+
+db = SQLAlchemy(app)
+now = datetime.now()
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+# models
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost")
+
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+
+#routes
+@app.route("/user", methods=["POST"])
+def create_user():
+    data = request.get_json()
+    new_user = User(
+        name = data["name"],
+        email = data["email"],
+        address = data["address"],
+        phone = data["phone"]
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({"message": "User created"}), 200
+
+
+@app.route("/user/descending_id", methods=["GET"])
+def get_all_user_descending():
+    pass
+
+
+@app.route("/user/ascending_id", methods=["GET"])
+def get_all_user_ascending():
+    pass
+
+
+@app.route("/user/<user_id>", methods=["GET"])
+def get_one_user(user_id):
+    pass
+
+
+@app.route("/user/<user_id>", methods=["DELETE"])
+def delete_one_user(user_id):
+    pass
+
+
+@app.route("/blog_post/<user_id>", methods=["POST"])
+def create_blog_post(user_id):
+    pass
+
+
+@app.route("/blog_post/<user_id>", methods=["GET"])
+def get_all_blog_posts(user_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["GET"])
+def get_one_blog_posts(blog_post_id):
+    pass
+
+
+@app.route("/blog_post/<blog_post_id>", methods=["DELETE"])
+def delete_one_blog_posts(blog_post_id):
+    pass
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
